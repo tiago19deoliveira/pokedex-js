a7168a8ccc81ffebd5fa2e0e587bbbb44152fef8
tiago19deoliveira <tiago18deoliveira@gmail.com>
2022-12-28 17:13:14 -0400

    committ, organizando listas pelo DO

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,55 @@
 console.log('cossante')
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonList(pokemon) {
+  return `
+  <li class="pokemon">
+          <span class="number">#001</span>
+          <span class="name">${pokemon.name}</span>
+
+          <div class="detail">
+            <ol class="types">
+              <li class="type">Grass</li>
+              <li class="type">Poison</li>
+            </ol>
+
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+              alt="${pokemon.name}"
+            />
+          </div>
+        </li>
+          
+  
+  `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+fetch(url)
+    .then((response) => {
+    return response.json()
+
+
+    .then((jsonBody) => {
+    console.log(jsonBody)
+
+
+    .then((pokemonList) => {
+
+      for (let i = 0; i < pokemonList.length; i++) {
+        const pokemon = pokemonList[i]
+        pokemonList.innerHTML += convertPokemonList(pokemon)
+      
+      }
+
+
+    })
+
+
+ .catch((error) => console.error(error))
+
+
